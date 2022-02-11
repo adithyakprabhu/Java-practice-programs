e028c961f5497123696a8efec3ca5cfe5e1db822
Adithya K Prabhu <66137659+adithyakprabhu@users.noreply.github.com>
2022-02-11 12:04:02 +0530

    Update code.java

--- 01-A Hello world program/Priority Queue/code.java
+++ 01-A Hello world program/Priority Queue/code.java
@@ -7,13 +7,13 @@ class PrioQueue
 		p.add(10);
 		p.add(20);
 		p.add(15);
-    p.add(30);
+    		p.add(30);
 		System.out.println("FIrst element "+p.peek());
 		System.out.println("Fetch and remove first"+p.poll());
-    System.out.println("After Removing top element");
+   		System.out.println("After Removing top element");
 		System.out.println(p.peek());
-    System.out.println("Does the PriorityQueue contain 20? :" +p.contains(20));
-    p.remove(30);
-    System.out.println("After removing 30 : "+p);
+  		System.out.println("Does the PriorityQueue contain 20? :" +p.contains(20));
+    		p.remove(30);
+    		System.out.println("After removing 30 : "+p);
 	}
 }
